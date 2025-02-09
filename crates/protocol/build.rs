b39140a7ee403abf0a0b0604e7856892598e65dd
zuiyu1998 <1542844298@qq.com>
2025-02-09 16:41:18 +0800

    feat: 添加msg proto

---
+++ crates/protocol/build.rs
@@ -0,0 +1,45 @@
+use std::process::Command;
+
+pub trait BuilderExt {
+    fn with_serde(self, path: &[&str]) -> Self;
+    fn with_hash_and_eq(self, path: &[&str]) -> Self;
+}
+
+impl BuilderExt for tonic_build::Builder {
+    fn with_serde(self, path: &[&str]) -> Self {
+        path.iter().fold(self, |acc, path| {
+            acc.type_attribute(path, "#[derive(serde::Serialize, serde::Deserialize)]")
+        })
+    }
+
+    fn with_hash_and_eq(self, path: &[&str]) -> Self {
+        path.iter().fold(self, |acc, path| {
+            acc.type_attribute(path, "#[derive(Hash, Eq)]")
+        })
+    }
+}
+
+fn main() {
+    tonic_build::configure()
+        .out_dir("src/pb")
+        .with_serde(&[])
+        .with_hash_and_eq(&[])
+        .compile_protos(
+            &[
+                "protos/wrapperspb.proto",
+                "protos/sdkws.proto",
+                "protos/conversation.proto",
+                "protos/msg.proto",
+            ],
+            &["protos"],
+        )
+        .unwrap();
+
+    // execute cargo fmt command
+    Command::new("cargo").arg("fmt").output().unwrap();
+
+    println!("cargo: rerun-if-changed=protocol/protos/wrapperspb.proto");
+    println!("cargo: rerun-if-changed=protocol/protos/sdkws.proto");
+    println!("cargo: rerun-if-changed=protocol/protos/conversation.proto");
+    println!("cargo: rerun-if-changed=protocol/protos/msg.proto");
+}
---
+++ crates/protocol/src/pb/mod.rs
@@ -0,0 +1,4 @@
+pub mod openim_conversation;
+pub mod openim_msg;
+pub mod openim_protobuf;
+pub mod openim_sdkws;
